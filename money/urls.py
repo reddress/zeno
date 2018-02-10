73563762fb5d41f9e4092b2ec4e0b11a45e1bce3
Heitor Chang <heitorchang@gmail.com>
2018-02-10 14:10:54 -0200

    separate views

--- money/urls.py
+++ money/urls.py
@@ -1,32 +1,59 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from . import views
+from .views import general
+from .views import account_type
+from .views import account
 
 app_name = 'money'
 
 urlpatterns = [
     path('',
-         views.IndexView.as_view(),
+         general.IndexView.as_view(),
          name='index'),
     
+    # AccountType
+
     path('accountType/',
-         login_required(views.AccountTypeList.as_view()),
+         login_required(account_type.AccountTypeList.as_view()),
          name='accountTypeList'),
     
     path('accountType/<int:pk>/',
-         login_required(views.AccountTypeDetail.as_view()),
+         login_required(account_type.AccountTypeDetail.as_view()),
          name='accountTypeDetail'),
     
     path('accountType/create/',
-         login_required(views.AccountTypeCreate.as_view()),
+         login_required(account_type.AccountTypeCreate.as_view()),
          name="accountTypeCreate"),
 
     path('accountType/update/<int:pk>/',
-         login_required(views.AccountTypeUpdate.as_view()),
+         login_required(account_type.AccountTypeUpdate.as_view()),
          name="accountTypeUpdate"),
 
     path('accountType/delete/<int:pk>/',
-         login_required(views.AccountTypeDelete.as_view()),
+         login_required(account_type.AccountTypeDelete.as_view()),
          name="accountTypeDelete"),
+
+    # Account
+
+    path('account/',
+         login_required(account.AccountList.as_view()),
+         name='accountList'),
+    
+    path('account/<int:pk>/',
+         login_required(account.AccountDetail.as_view()),
+         name='accountDetail'),
+    
+    path('account/create/',
+         login_required(account.AccountCreate.as_view()),
+         name="accountCreate"),
+
+    path('account/update/<int:pk>/',
+         login_required(account.AccountUpdate.as_view()),
+         name="accountUpdate"),
+
+    path('account/delete/<int:pk>/',
+         login_required(account.AccountDelete.as_view()),
+         name="accountDelete"),
+
 ]
--- money/views/account_type.py
+++ money/views/account_type.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import AccountType, Account
+from ..models import AccountType
 
 class IndexView(TemplateView):
     template_name = "money/index.html"
@@ -47,39 +46,3 @@ class AccountTypeDelete(DeleteView):
 
     def get_queryset(self):
         return AccountType.objects.filter(owner=self.request.user, pk=self.kwargs['pk'])
-
-
-# Account
-    
-class AccountList(ListView):
-    def get_queryset(self):
-        return Account.objects.filter(owner=self.request.user)
-
-
-class AccountDetail(DetailView):
-    def get_queryset(self):
-        return Account.objects.filter(owner=self.request.user, pk=self.kwargs['pk'])
-    
-
-class AccountCreate(CreateView):
-    model = Account
-    fields = ['account_type', 'name', 'abbreviation']
-
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
-
-class AccountUpdate(UpdateView):
-    model = Account
-    fields = ['account_type', 'name', 'abbreviation']
-
-    def get_queryset(self):
-        return Account.objects.filter(owner=self.request.user, pk=self.kwargs['pk'])
-
-
-class AccountDelete(DeleteView):
-    model = Account
-    success_url = reverse_lazy('money:accountList')
-
-    def get_queryset(self):
-        return Account.objects.filter(owner=self.request.user, pk=self.kwargs['pk'])
