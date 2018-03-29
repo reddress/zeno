8fbb24c75156ac500597b7f3fc754a3f75864c6e
Heitor Chang <heitorchang@gmail.com>
2018-03-29 20:08:27 -0300

    add dollar sign

--- movimento/templatetags/currency_filters.py
+++ movimento/templatetags/currency_filters.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter(needs_autoescape=True)
 def currencyCents(value, autoescape=True):
-    return "{:,.2f}".format(value / 100.0).translate(str.maketrans(",.", ".,"))
+    return "$ {:,.2f}".format(value / 100.0).translate(str.maketrans(",.", ".,"))
--- registros/templatetags/currency_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(needs_autoescape=True)
-def currencyCents(value, autoescape=True):
-    return "{:,.2f}".format(value / 100.0).translate(str.maketrans(",.", ".,"))
