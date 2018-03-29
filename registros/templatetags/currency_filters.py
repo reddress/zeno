51e29194417dbe16848eaba9b392bdadfba5eb68
Heitor Chang <heitorchang@gmail.com>
2018-03-29 20:04:39 -0300

    currency cents filter

---
+++ registros/templatetags/currency_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+def currencyCents(value, autoescape=True):
+    return "{:,.2f}".format(value / 100.0).translate(str.maketrans(",.", ".,"))
